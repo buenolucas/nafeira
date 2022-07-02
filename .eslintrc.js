50fbcf5c69c03ae689b088b2a268c989f6414be9
Lucas Bueno <buenolucasbr@gmail.com>
2022-07-02 01:08:53 -0300

    base setuo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,52 @@
 module.exports = {
   root: true,
-  // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  plugins: [
+    "@typescript-eslint",
+    "jest",
+    "react",
+    "testing-library",
+    "jest-dom",
+    "use-encapsulation",
+    //"@washingtonpost/wpds",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "plugin:react/recommended",
+    "plugin:testing-library/react",
+    "plugin:testing-library/dom",
+    "plugin:jest-dom/recommended",
+    "plugin:storybook/recommended",
+  ],
+  overrides: [
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      extends: ["plugin:testing-library/react"],
+    },
+    {
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off",
+        //"@washingtonpost/wpds/theme-colors": "warn",
+      },
+    },
+  ],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
   settings: {
-    next: {
-      rootDir: ["apps/*/"],
+    react: {
+      version: "detect",
     },
   },
 };
